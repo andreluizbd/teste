7c09168f125c295668126c7f43344464ecea423f
Andre Brito <andreluizbd@gmail.com>
2016-02-02 12:00:22 -0200

    primeiro commit

---
+++ src/main/java/br/com/javamagazine/springboot_test/App.java
@@ -0,0 +1,13 @@
+package br.com.javamagazine.springboot_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
